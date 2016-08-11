8a28553112173c75d60394d2e596663fce0b590d
renanbronchart <renan.bronchart@gmail.com>
2016-08-11 14:10:08 +0200

    [Frontend] Create layout and base js

---
+++ js/activity_one.js
@@ -0,0 +1,15 @@
+var createForm = {
+  init: function () {
+    var self = $(this);
+
+    $('.init-form').on('click', function () {
+
+      self.creation();
+
+    })
+  },
+
+  creation: function() {
+
+  }
+}
