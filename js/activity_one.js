4233ad0fd47aa2c18d2a243aa3dc3821798902b5
renanbronchart <renan.bronchart@gmail.com>
2016-08-22 14:20:13 +0200

    [Frontend] Fix interaction aside to create form

--- js/activity_one.js
+++ js/activity_one.js
@@ -1,15 +1,21 @@
 var createForm = {
   init: function () {
-    var self = $(this);
-
-    $('.init-form').on('click', function () {
+    var self = this;
 
       self.creation();
-
-    })
   },
 
   creation: function() {
+    $('.init-form').on('click', function () {
+      var attribute = $(this).attr('data-name'),
+          formGroup = $('aside .form-group');
+
+      formGroup.find('label').attr('for', attribute).empty().append(attribute)
+      formGroup.addClass('active');
+
+    })
 
   }
 }
+
+createForm.init();
