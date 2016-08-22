16238656ddb1223b5a1b9d1d12147a112a9d129e
Renan Bronchart <renan.bronchart@gmail.com>
2016-08-22 23:58:09 +0200

    [Frontend] Fix new formulaire

--- js/activity_one.js
+++ js/activity_one.js
@@ -2,20 +2,52 @@ var createForm = {
   init: function () {
     var self = this;
 
-      self.creation();
+      self.build();
   },
 
-  creation: function() {
+  build: function() {
+    var self = this,
+        $formGroup = $('aside .form-group');
+
     $('.init-form').on('click', function () {
-      var attribute = $(this).attr('data-name'),
-          formGroup = $('aside .form-group');
+      var attribute = $(this).attr('data-name');
 
-      formGroup.find('label').attr('for', attribute).empty().append(attribute)
-      formGroup.addClass('active');
-
+      $formGroup.find('label').attr('for', attribute).empty().append(attribute);
+      $formGroup.find('input').val('');
+      $formGroup.addClass('active');
     })
 
+    self.creation($formGroup);
+
+  },
+
+  creation: function ($formGroup) {
+    $formGroup.find('button.validate').on('click', function () {
+      var valInput = $formGroup.find('input').val(),
+          attribute = $formGroup.find('label').attr('for'),
+          $sectionLeft = $('section.left');
+
+      if (attribute == 'label') {
+        var label = $sectionLeft.find('.form-group').append('<label></label>');
+
+        label.text(valInput);
+      }else if(attribute == 'input'){
+        var $input = '<input id=""/>';
+
+
+        if ($sectionLeft.find('.form-group label').length >= 1) {
+          $sectionLeft.find('.form-group label').last().after($input)
+
+        } else {
+          $sectionLeft.find('.form-group').append($input);
+        }
+
+        $sectionLeft.find('.form-group input').attr('id', valInput);
+      }
+
+    })
   }
+
 }
 
 createForm.init();
